383675cf1eea5f95d1fb0aafe5d11427d70b83ab
Roma <wrongmanua@gmail.com>
2015-09-19 20:50:20 +0300

    ticket:1 update urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'hello.views.devinfo_view', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
