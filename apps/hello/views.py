fae0f50d7ed70873a94e3ed712ce95f5cb3edec2
Roma <wrongmanua@gmail.com>
2015-09-19 20:19:52 +0300

    ticket:1 update views.py

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from hello.models import Devinfo
 
-# Create your views here.
+""" Subscribe developer information view to return all fields of the models into the html page """
+
+def devinfo_view(request):
+	devinfo_data = Devinfo.objects.get(pk=1)
+	return render(request,'home.html',{"devinfo_data": devinfo_data})
