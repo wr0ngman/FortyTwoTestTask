de559d6e2a136cce2b6d3000a68c954a293e9699
Roma <wrongmanua@gmail.com>
2015-09-19 20:15:16 +0300

    ticket:1 Update models.py

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+""" Subscribe developer information model fields """
+class Devinfo(models.Model):
+    name = models.CharField(max_length=70)
+    lastname = models.CharField(max_length=70)
+    birthdate = models.DateField()
+    bio = models.TextField()    
+    email = models.EmailField()
+    jabber = models.CharField(max_length=70)
+    skype = models.CharField(max_length=70)
+    othcontacts = models.TextField()
+    
+    def __unicode__(self):
+        return u'Information about: %s %s email:%s ;jabber:%s'%(self.name, self.lastname, self.email, self.jabber)
