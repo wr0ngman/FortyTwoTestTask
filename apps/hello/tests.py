66083354ec87e3520ce5dd123356fead778e8483
Roma <wrongmanua@gmail.com>
2015-09-19 20:16:30 +0300

    ticket:1 update tests.py

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,24 @@
+from apps.hello import models
 from django.test import TestCase
 
-# Create your tests here.
 
-
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+class HelloTests(TestCase):
+    """
+    create a test record into base
+    """    
+    def test_devinfo_displayed(self):            
+        models.Devinfo.objects.create(name='Roma',
+            lastname='Shevchuk',
+            birthdate='1985-05-17', bio='...',
+            email='wrongman@gmail.com',
+            jabber='wr0ngman@khavr.com',
+            skype='wr0ngman',
+            othcontacts='+380989682061')
+        dev = models.Devinfo.objects.get(pk=1)
+        assert(dev.name == u'Roma')
+        assert(dev.lastname == u'Shevchuk')
+        assert(dev.birthdate.strftime('%Y-%m-%d') == u'1985-05-17')
+        assert(dev.email == u'wrongman@gmail.com')
+        assert(dev.jabber == u'wr0ngman@khavr.com')
+        assert(dev.skype == u'wr0ngman')
+        assert(dev.othcontacts == u'+380989682061')
