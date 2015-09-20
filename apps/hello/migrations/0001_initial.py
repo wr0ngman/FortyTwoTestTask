a854c70401cede05079b41d489c157dfa5581053
Roma <wrongmanua@gmail.com>
2015-09-20 20:39:52 +0300

    ticket:1 update 0001_initial.py

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -11,13 +11,13 @@ class Migration(SchemaMigration):
         # Adding model 'Devinfo'
         db.create_table(u'hello_devinfo', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('lastname', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=70)),
+            ('lastname', self.gf('django.db.models.fields.CharField')(max_length=70)),
             ('birthdate', self.gf('django.db.models.fields.DateField')()),
             ('bio', self.gf('django.db.models.fields.TextField')()),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('skype', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=70)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=70)),
             ('othcontacts', self.gf('django.db.models.fields.TextField')()),
         ))
         db.send_create_signal(u'hello', ['Devinfo'])
@@ -35,11 +35,11 @@ class Migration(SchemaMigration):
             'birthdate': ('django.db.models.fields.DateField', [], {}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'lastname': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'lastname': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
             'othcontacts': ('django.db.models.fields.TextField', [], {}),
-            'skype': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '70'})
         }
     }
 
