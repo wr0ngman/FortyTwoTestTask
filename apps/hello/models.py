4b009cbfcd1dd4e4565b8a2696adfea57d3b8a87
Roma <wrongmanua@gmail.com>
2015-09-22 18:57:53 +0300

    ticket:1 update common.py(reformat to PEP8)

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 
 """ Subscribe developer information model fields """
+
+
 class Devinfo(models.Model):
     name = models.CharField(max_length=70)
     lastname = models.CharField(max_length=70)
     birthdate = models.DateField()
-    bio = models.TextField()    
+    bio = models.TextField()
     email = models.EmailField()
     jabber = models.CharField(max_length=70)
     skype = models.CharField(max_length=70)
     othcontacts = models.TextField()
-    
+
     def __unicode__(self):
-        return u'Information about: %s %s email:%s ;jabber:%s'%(self.name, self.lastname, self.email, self.jabber)
+        return u'Information about: %s %s email:%s ;jabber:%s' % (self.name, self.lastname, self.email, self.jabber)
