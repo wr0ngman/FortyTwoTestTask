f5697f21761a720b19581109fd0f7d14741c8b0d
Roma <wrongmanua@gmail.com>
2015-09-22 18:59:34 +0300

    ticket:1 update tests.py(reformat to PEP8)

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -3,17 +3,19 @@ from django.test import TestCase
 
 
 class HelloTests(TestCase):
+
     """
     create a test record into base
-    """    
-    def test_devinfo_displayed(self):            
+    """
+
+    def test_devinfo_displayed(self):
         models.Devinfo.objects.create(name='Roma',
-            lastname='Shevchuk',
-            birthdate='1985-05-17', bio='...',
-            email='wrongman@gmail.com',
-            jabber='wr0ngman@khavr.com',
-            skype='wr0ngman',
-            othcontacts='+380989682061')
+                                      lastname='Shevchuk',
+                                      birthdate='1985-05-17', bio='...',
+                                      email='wrongman@gmail.com',
+                                      jabber='wr0ngman@khavr.com',
+                                      skype='wr0ngman',
+                                      othcontacts='+380989682061')
         dev = models.Devinfo.objects.get(pk=1)
         assert(dev.name == u'Roma')
         assert(dev.lastname == u'Shevchuk')
