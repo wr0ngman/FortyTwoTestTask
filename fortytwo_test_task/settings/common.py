834ed7d0ed98ed08494c3695a3ed53f2fb3ea5a8
Roma <wrongmanua@gmail.com>
2015-09-22 12:25:32 +0300

    ticket:1 update common.py

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.hello'
-    #'south'
+    'apps.hello',
+    'south'
 
 )
 
